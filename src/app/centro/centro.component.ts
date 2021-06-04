811016ee1afc44837a010aac0a65b93ebd0eaa89
Ana Isabel Sanz <sansasan@jcyl.es>
2021-06-04 12:48:17 +0200

    ejercicio 9

---
+++ src/app/centro/centro.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-centro',
+  template: `<h2>Centros</h2>
+             <p>Nombre Centro: {{centro.nombre}}</p>
+             <p>Dirección Centro: {{centro.direccion}}</p>`,
+  styleUrls: ['./centro.component.css']
+})
+export class CentroComponent implements OnInit {
+
+  centro = {nombre:"CEIP Antonio García Quintana", direccion:"Plaza España"};
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
