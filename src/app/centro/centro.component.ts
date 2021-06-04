6d31d66315eb139c554ddc1fa14951e75b87e9c5
Ana Isabel Sanz <sansasan@jcyl.es>
2021-06-04 13:21:11 +0200

    ejercicio 10

--- src/app/centro/centro.component.ts
+++ src/app/centro/centro.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-centro',
@@ -15,5 +15,7 @@ export class CentroComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+  @Input() centroSolicitud: any;
 
 }
